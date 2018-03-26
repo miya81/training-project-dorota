65740c3235ee7500f69024e008a0402a38f8c286
Dorota Rychlinska <dorota.rychlinska@globallogic.com>
2018-03-26 12:25:21 +0200

    My first commit

---
+++ src/main/java/demo/dao/PlayerDao.java
@@ -0,0 +1,13 @@
+package demo.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import demo.domain.Player;
+
+@RestResource(path="players", rel="players")
+public interface PlayerDao extends CrudRepository<Player, Long> {
+
+}
